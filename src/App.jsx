853a28d04a265cc42c9284da3fb0890c5eab5108
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-30 15:30:12 +0300

    map over data elements to create a new array of card elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Header from './components/Header.jsx';
+import Card from './components/Card.jsx';
+import Destinations from './Data';
 import './App.css';
 
-const App = () => (
-  <section>
-    <Header />
-  </section>
-);
+const App = () => {
+  const travelCards = Destinations.map((destination) => (
+    <Card travel={destination} />
+  ));
+
+  return (
+    <section>
+      <Header />
+      {/* {travelCards} */}
+    </section>
+  );
+};
 
 export default App;
