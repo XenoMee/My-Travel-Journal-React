93d8654792aea57eaa00db47e18f619074ea8a42
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-30 11:57:50 +0300

    create Header html structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => (
+  <header>
+    <div className='header-content'>
+      <i class='fa-solid fa-earth-europe'></i>
+      <h1>Adrian's Travel Journal</h1>
+    </div>
+  </header>
+);
+
+export default Header;
