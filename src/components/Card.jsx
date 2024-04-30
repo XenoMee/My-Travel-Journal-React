41b4a9280b49f5bb22543a2602529920bc829531
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-30 15:28:45 +0300

    create travel card component structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
 
-const Card = () => {};
+const Card = (props) => (
+  <div className='travel-card grid-flow'>
+    <div className='card-content grid-flow'>
+      <div className='card-header'>
+        <h2 className='card-title'>{props.travel.title}</h2>
+        <div className='card-location'>
+          <div className='flex-group'>
+            <i class='fa-solid fa-location-dot'></i>
+            <p className='card-country'>{props.travel.location}</p>
+          </div>
+          <a href={`${props.travel.googleMapsUrl}`}>View on Google Maps</a>
+        </div>
+      </div>
+
+      <div className='card-body'>
+        <span className='card-date'>
+          {props.travel.startDate} - {props.travel.endDate}
+        </span>
+        <p className='card-description'>{props.travel.description}</p>
+      </div>
+    </div>
+
+    <img
+      src={`${props.travel.imageUrl}`}
+      alt={`${props.travel.title}`}
+      className='card-image'
+    />
+  </div>
+);
 
 export default Card;
