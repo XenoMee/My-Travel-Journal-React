0d65d1e5200ad53c510953b5cdb27f7847f1ca69
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-30 17:36:01 +0300

    add travel destination data inside Data.js file

--- src/Data.js
+++ src/Data.js
@@ -1,12 +1,82 @@
 export default [
   {
-    title: 'Mount Fuji',
-    location: 'Japan',
-    googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
-    startDate: '12 Jan, 2021',
-    endDate: '24 Jan, 2021',
+    title: "St. Stephen's Cathedral",
+    location: 'Vienna',
+    googleMapsUrl: 'https://maps.app.goo.gl/sCMhNKufAWY4Bqgz7',
+    startDate: '26 Dec 2021',
+    endDate: '30 Dec 2021',
     description:
-      'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
-    imageUrl: 'https://source.unsplash.com/WLxQvbMyfas',
+      "St. Stephen's Cathedral is the symbol of Vienna and also the very center of the city. At 136 meters in height, it is the tallest church in Austria.",
+    imageUrl: '../images/st_stephens_cathedral.jpg',
+  },
+  {
+    title: 'Eiffle Tower',
+    location: 'France',
+    googleMapsUrl: 'https://maps.app.goo.gl/S8QU5FmpJ3ip199w5',
+    startDate: '13 Apr 2022',
+    endDate: '17 Apr 2022',
+    description:
+      "The Eiffel Tower in Paris, France, has serious celeb status. Completed in March 1889, it's one of the most recognizable monuments in the world. And the site, which is also known as the Iron Lady, receives nearly seven millions tourists a year.",
+    imageUrl: '../images/eiffel_tower.jpg',
+  },
+  {
+    title: 'Duomo di Milano',
+    location: 'Italy',
+    googleMapsUrl: 'https://maps.app.goo.gl/a7F1vwY4pggTd3qn6',
+    startDate: '02 Aug 2022',
+    endDate: '11 Aug 2022',
+    description:
+      'The Duomo of Milan,the cathedral heart of the city, is the largest and most complex Gothic building in Italy, made of pink white marble that comes from a quarry dedicated only to this building. It is 157 meters long and on the main spire, 108.5 meters high, stands the golden statue of the Madonnina, a symbol loved by all Milanese and beyond.',
+    imageUrl: '../images/duomo_di_milano.jpg',
+  },
+  {
+    title: 'Big Ben',
+    location: 'United Kingdom',
+    googleMapsUrl: 'https://maps.app.goo.gl/ZKYUjXihyUC7qMRt6',
+    startDate: '27 Dec 2022',
+    endDate: '30 Dec 2022',
+    description:
+      "The Houses of Parliament and Elizabeth Tower, commonly called Big Ben, are among London's most iconic landmarks and must-see London attractions. Technically, Big Ben is the name given to the massive bell inside the clock tower, which weighs more than 13 tons (13,760 kg).",
+    imageUrl: '../images/big_ben.jpg',
+  },
+  {
+    title: 'Acropolis',
+    location: 'Greece',
+    googleMapsUrl: 'https://maps.app.goo.gl/QaK7HtGmyhwsk7XN7',
+    startDate: '06 Jul 2023',
+    endDate: '11 Jul 2023',
+    description:
+      'The Acropolis of Athens and its monuments are universal symbols of the classical spirit and civilization and form the greatest architectural and artistic complex bequeathed by Greek Antiquity to the world.',
+    imageUrl: '../images/acropolis.jpg',
+  },
+  {
+    title: 'Blue Eye',
+    location: 'Albania',
+    googleMapsUrl: 'https://maps.app.goo.gl/J52eGwHffma5ukt86',
+    startDate: '11 Aug 2023',
+    endDate: '17 Aug 2023',
+    description:
+      'The Blue Eye is a water spring and initial water source of the Bistricë, a 25 km long river which ends in the Ionian Seat just south of Saranda. The source is a natural phenomenon with a depth of at least 50 meters, the deepest any diver has been able to go, so the actual depth is even unknown.',
+    imageUrl: '../images/blue_eye_ksamil.jpg',
+  },
+  {
+    title: 'Colosseum',
+    location: 'Italy',
+    googleMapsUrl: 'https://maps.app.goo.gl/8URP8Ga4aJvLxfiw8',
+    startDate: '23 Sep 2023',
+    endDate: '26 Sep 2023',
+    description:
+      'The Colosseum, also named the Flavian Amphitheater, is a large amphitheater in Rome. It was built during the reign of the Flavian emperors as a gift to the Roman people.',
+    imageUrl: '../images/colosseum.jpg',
+  },
+  {
+    title: 'Promenade des Anglais',
+    location: 'France',
+    googleMapsUrl: 'https://maps.app.goo.gl/mjDNgsVGok4w875k9',
+    startDate: '11 Apr 2024',
+    endDate: '14 Apr 2024',
+    description:
+      'Promenade des Anglais in Nice is the most important attraction in the city. It stretches for seven kilometers and provides an uninterrupted view of the azure sea and palm trees.',
+    imageUrl: '../images/promenade_des_anglais.jpg',
   },
 ];
