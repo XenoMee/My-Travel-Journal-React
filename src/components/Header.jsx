10e9c65b4704a49807422d45be33a1c7e5442b92
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-04-30 15:57:24 +0300

    add flex-group utility to class to header content div

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => (
   <header>
-    <div className='header-content'>
+    <div className='header-content flex-group'>
       <i class='fa-solid fa-earth-europe'></i>
       <h1 className='header-title'>Adrian's Travel Journal</h1>
     </div>
