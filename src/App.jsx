396f0ab1d747b6a8a1b7f2be16669316d4b41779
Adrian Trandafir <trandafiradrian98@gmail.com>
2024-06-20 12:37:10 +0300

    update App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import './App.css';
 
 const App = () => {
   const travelCards = Destinations.map((destination) => (
-    <Card travel={destination} />
+    <Card travel = {destination} />
   ));
 
   return (
